425ea7cf1ce6975acd9f2bd45bcb9dec4f459ace
kr-chirag <chirag.p@knackroot.com>
2025-04-16 12:52:07 +0530

    fix: hashing

--- ethers/src/script.ts
+++ ethers/src/script.ts
@@ -2,7 +2,7 @@ import { ethers } from "ethers";
 import { PRIVATE_KEY1, RPC_INFURA_SEPOLIA } from "./config";
 import ABI from "./ABICommitReveal.json";
 
-const contractAdress = "0xb6C565633B5417BC4f5C0434F988B80e1f723E77";
+const contractAdress = "0x5Bc5338F4f0f529F238d9cdb429bfF4eC095467d";
 
 async function main() {
     const provider = new ethers.JsonRpcProvider(RPC_INFURA_SEPOLIA);
@@ -10,16 +10,17 @@ async function main() {
     const commitReveal = new ethers.Contract(contractAdress, ABI, signer);
 
     const msg = ethers.toUtf8Bytes("My Message");
-    const commit = await commitReveal.commit(msg);
 
-    const sign = signer.signingKey.sign(commit);
+    const sign = await signer.signMessage(msg);
     const { v, r, s } = ethers.Signature.from(sign);
+    // console.log(r + s.slice(2) + v.toString(16));
 
     const result = await commitReveal.verify(msg, v, r, s);
     console.log(result);
 
-    const result2 = await commitReveal.verify("0x1234", v, r, s);
-    console.log(result2);
+    // this will revert
+    // const result2 = await commitReveal.verify("0x1234", v, r, s);
+    // console.log(result2);
 }
 
 main().catch(console.log);
--- solidity/contracts/CommitReveal.sol
+++ solidity/contracts/CommitReveal.sol
@@ -1,14 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-contract CommitReveal {
+import "@openzeppelin/contracts/utils/Strings.sol";
 
+contract CommitReveal {
+    using Strings for uint256;
     error InvalidSigner(address signer, address sender);
+    error InvalidSign(bytes error);
 
     constructor() {}
 
     function hash(bytes memory data) public pure returns (bytes32) {
-        return keccak256(data);
+        return
+            keccak256(
+                abi.encodePacked(
+                    "\x19Ethereum Signed Message:\n",
+                    data.length.toString(),
+                    data
+                )
+            );
     }
 
     function verify(
@@ -19,7 +29,6 @@ contract CommitReveal {
     ) public view {
         bytes32 commitHash = hash(data);
         address signer = ecrecover(commitHash, v, r, s);
-        require(signer == msg.sender, InvalidSigner(signer,msg.sender));
+        require(signer == msg.sender, InvalidSigner(signer, msg.sender));
     }
-
 }
